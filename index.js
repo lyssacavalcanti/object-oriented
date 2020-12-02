334895126f37a2a51344cca1290f560c2b3c76ab
lyssacavalcanti <lyssa.cavalcanti@gmail.com>
2020-12-02 20:15:46 -0300

    Created a class to insert infos about agency and balance.

--- index.js
+++ index.js
@@ -1,21 +1,37 @@
+// Create a class that works as a pattern to be followed.
 class Cliente {
     nome;
     cpf;
-    agencia;
-    saldo;
 }
 
+class  ContaCorrente {
+    agencia;
+    saldo;
+
+     sacar(valor){
+         if(this.saldo >= valor) {
+             this.saldo -= valor;
+         }
+     }
+}
+
+// Use the Class Cliente to create a new register of client inside a const.
 const cliente1 = new Cliente();
 cliente1.nome = "Ricardo";
 cliente1.cpf = 11155577788;
-cliente1.agencia = 1001;
-cliente1.saldo = 0;
 
 const cliente2 = new Cliente();
 cliente2.nome = "Rosa";
 cliente2.cpf = 33355577782;
-cliente2.agencia = 1001;
-cliente2.saldo = 0;
 
+const contaCorrenteRicardo = new ContaCorrente();
+contaCorrenteRicardo.agencia = 1001;
+contaCorrenteRicardo.saldo = 0;
+
+console.log(contaCorrenteRicardo.saldo);
+contaCorrenteRicardo.saldo = 100;
+console.log(contaCorrenteRicardo.saldo);
+contaCorrenteRicardo.sacar(50);
+console.log(contaCorrenteRicardo.saldo);
 console.log(cliente1);
 console.log(cliente2);
